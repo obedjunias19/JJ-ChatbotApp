57533cc2833bc750d15d9b77e70ee0545b8be8a5
Obed Junias <49679218+obedjunias19@users.noreply.github.com>
2023-11-24 17:21:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,16 +9,6 @@ df = pd.read_json('./chatbot-intents.json')
 intents = df.intents.values
 
 def main():
-    global counter
-    assistant_response = ""
-    
-    st.set_page_config(
-        page_title="Jolly Junias - Feel Good Chatbot",
-        page_icon="J❤️J",
-        layout="wide",
-        initial_sidebar_state="expanded",
-    )
-    
     st.title("Jolly Junias - Feel Good Chatbot 😊")
     
     # Initialize chat history
@@ -54,20 +44,6 @@ def main():
         # Add assistant response to chat history
         st.session_state.messages.append({"role": "assistant", "content": full_response})
     
-   
-    # st.sidebar.image("hhttps://cdn.pixabay.com/photo/2023/02/05/20/01/ai-generated-7770474_960_720.png", use_container_width=True)
-    # st.write("Don't Wait!!! Just start typing to start the conversation.")
-
-    # counter += 1
-    # user_input = st.text_input("You:", key=f"user_input_{counter}")
-
-    # if user_input:
-    #     response = chatbot(user_input,intents)
-    #     st.text_area("JJ:", value=response, height=100, max_chars=None, key=f"chatbot_response_{counter}")
-
-    #     if response.lower() in ['goodbye', 'bye']:
-    #         st.write("Thank you for chatting with me. Have a great day!")
-    #         st.stop()
 
 if __name__ == '__main__':
     main()
