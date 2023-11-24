215d19114c19665e984f64a674aac9694eb3a13b
Obed Junias <49679218+obedjunias19@users.noreply.github.com>
2023-11-24 17:13:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,31 +32,36 @@ def main():
 
     # Accept user input
     if prompt := st.chat_input("What is up?"):
+
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+    
         # Display user message in chat message container
         with st.chat_message("user"):
             st.markdown(prompt)
+            
         assistant_response = chatbot(prompt,intents)
-        print(assistant_response)
+        # print(assistant_response)
         # Add user message to chat history
         st.session_state.messages.append({"role": "user", "content": prompt})
 
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-        
-        if assistant_response.lower() in ['goodbye', 'bye']:
-            assistant_response = "Thank you for chatting with me. Have a great day!"
-        
-        # # Simulate stream of response with milliseconds delay
-        for chunk in assistant_response.split():
-            full_response += chunk + " "
-            time.sleep(0.05)
-            # Add a blinking cursor to simulate typing
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+            
+            if assistant_response.lower() in ['goodbye', 'bye']:
+                assistant_response = "Thank you for chatting with me. Have a great day!"
+            
+            # # Simulate stream of response with milliseconds delay
+            for chunk in assistant_response.split():
+                full_response += chunk + " "
+                time.sleep(0.05)
+                # Add a blinking cursor to simulate typing
+                message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
     
    
     # st.sidebar.image("hhttps://cdn.pixabay.com/photo/2023/02/05/20/01/ai-generated-7770474_960_720.png", use_container_width=True)
