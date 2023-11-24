bde0532813a5d95a7172406ca1783afcd44ae05b
Obed Junias <49679218+obedjunias19@users.noreply.github.com>
2023-11-24 17:09:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,11 @@ def main():
     # Initialize chat history
     if "messages" not in st.session_state:
         st.session_state.messages = []
+    
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
     # Accept user input
     if prompt := st.chat_input("What is up?"):
